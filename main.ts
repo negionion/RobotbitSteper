9fed80a5c1878d04a5024f48df1c395c0173e7d0
negionion <31944608+negionion@users.noreply.github.com>
2019-03-17 15:09:53 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -24,17 +24,17 @@ namespace robotbit {
     const ALL_LED_OFF_L = 0xFC
     const ALL_LED_OFF_H = 0xFD
 
-    const STP_CHA_L = 2047
-    const STP_CHA_H = 4095
+    const STP_CHA_L = (2047 / 2)
+    const STP_CHA_H = (4095 / 2)
 
     const STP_CHB_L = 1
-    const STP_CHB_H = 2047
+    const STP_CHB_H = (2047 / 2)
 
-    const STP_CHC_L = 1023
-    const STP_CHC_H = 3071
+    const STP_CHC_L = (1023 / 2)
+    const STP_CHC_H = (3071 / 2)
 
-    const STP_CHD_L = 3071
-    const STP_CHD_H = 1023
+    const STP_CHD_L = (3071 / 2)
+    const STP_CHD_H = (1023 / 2)
 
     // HT16K33 commands
     const HT16K33_ADDRESS = 0x70
