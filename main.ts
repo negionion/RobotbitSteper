68fa1efc8acabaf60c2fc97c5a4a814468a60a3c
negionion <31944608+negionion@users.noreply.github.com>
2019-03-17 19:47:14 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -24,17 +24,17 @@ namespace robotbit {
     const ALL_LED_OFF_L = 0xFC
     const ALL_LED_OFF_H = 0xFD
 
-    const STP_CHA_L = (2047 / 2)
-    const STP_CHA_H = (4095 / 2)
+    const STP_CHA_L = 2047
+    const STP_CHA_H = 4095
 
     const STP_CHB_L = 1
-    const STP_CHB_H = (2047 / 2)
+    const STP_CHB_H = 2047
 
-    const STP_CHC_L = (1023 / 2)
-    const STP_CHC_H = (3071 / 2)
+    const STP_CHC_L = 1023
+    const STP_CHC_H = 3071
 
-    const STP_CHD_L = (3071 / 2)
-    const STP_CHD_H = (1023 / 2)
+    const STP_CHD_L = 3071
+    const STP_CHD_H = 1023
 
     // HT16K33 commands
     const HT16K33_ADDRESS = 0x70
@@ -113,7 +113,7 @@ namespace robotbit {
 
     function initPCA9685(): void {
         i2cwrite(PCA9685_ADDRESS, MODE1, 0x00)
-        setFreq(50);
+        setFreq(100); //50 to 100
         for (let idx = 0; idx < 16; idx++) {
             setPwm(idx, 0 ,0);
         }
@@ -201,7 +201,7 @@ namespace robotbit {
     //% blockId=debugLog block="Debug"
     //% weight=90
     export function debugLog(): number {
-        return 6 / 2;
+        return 8 / 2;
     }
 
     /**
